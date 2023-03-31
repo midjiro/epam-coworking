d95e88d996bae4a21baf80677a4afbf05bba8568
Mikhail Gulak <miha.gulak@gmail.com>
2023-03-31 09:01:47 +0300

    add booking form

--- main.js
+++ main.js
@@ -13,3 +13,43 @@ window.addEventListener('resize', (e) => {
 		navbarBtn.classList.remove('navbar__btn--active');
 	}
 });
+
+function extract_form_data(fields) {
+	let data = {};
+	fields.forEach((field) => {
+		const { name, value } = field;
+
+		if (field.getAttribute('type') == 'radio') {
+			if (!field.checked) {
+				return;
+			}
+
+			data[name] = value;
+		}
+
+		if (field.localName == 'input') {
+			data[name] = value;
+		}
+	});
+
+	return data;
+}
+
+document.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const fields = e.target.querySelectorAll('input');
+
+	let data = extract_form_data(fields);
+
+	const message = `Congratulations, ${
+		data['full-name']
+	}! You have successfully reserved a seat in the ${data['coworking-type']
+		.split('-')
+		.join(
+			' '
+		)}. Soon you will receive a letter with the seat number for this phone: ${
+		data['phone']
+	}.`;
+
+	alert(message);
+});
