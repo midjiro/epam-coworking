5527b77af5ee9a593d5c9e1e068b18c4345839d1
Mikhail Gulak <miha.gulak@gmail.com>
2023-04-07 08:26:08 +0300

    update styles and script for navbar menu button

--- main.js
+++ main.js
@@ -1,16 +1,53 @@
-const navbarBtn = document.querySelector('.navbar__btn');
-const navbarMenu = document.querySelector('.navbar__menu');
+let button = document.getElementsByClassName('navbar__toggle')[0];
 
-navbarBtn.addEventListener('click', (e) => {
-	navbarMenu.classList.toggle('navbar__menu--active');
-	navbarBtn.classList.toggle('navbar__btn--active');
+function getMenu(button) {
+	let identifier = button.getAttribute('aria-controls');
+	if (!identifier) {
+		throw new Error('No target specified for menu button.');
+		return;
+	}
+
+	let menu = document.getElementById(identifier);
+	return menu;
+}
+
+function isExpanded(button) {
+	let isExpanded = button.getAttribute('aria-expanded');
+	return isExpanded;
+}
+
+function toggleMenuVisibility(button, menu) {
+	if (!menu) {
+		throw new Error('No menu element provided.');
+		return;
+	}
+
+	let expanded = isExpanded(button);
+	if (expanded === 'false') {
+		button.setAttribute('aria-expanded', 'true');
+	} else {
+		button.setAttribute('aria-expanded', 'false');
+	}
+}
+
+button.addEventListener('click', (e) => {
+	let menu = getMenu(e.currentTarget);
+	toggleMenuVisibility(button, menu);
 });
 
-window.addEventListener('resize', (e) => {
-	const width = window.innerWidth;
-	if (width > 768 && navbarMenu.classList.contains('navbar__menu--active')) {
-		navbarMenu.classList.remove('navbar__menu--active');
-		navbarBtn.classList.remove('navbar__btn--active');
+window.addEventListener('resize', () => {
+	let width = window.innerWidth;
+	if (width >= 768) {
+		if (isExpanded(button) === 'false') {
+			button.setAttribute('aria-expanded', 'true');
+		}
+	}
+});
+
+window.addEventListener('load', () => {
+	let width = window.innerWidth;
+	if (width >= 768) {
+		button.setAttribute('aria-expanded', 'true');
 	}
 });
 
