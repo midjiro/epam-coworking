0b61609ca9988b04bee925d760e37a07dbbfa9f4
midjiro <miha.gulak@gmail.com>
2023-05-19 10:57:32 +0300

    update navigation links

--- scripts/general.js
+++ scripts/general.js
@@ -2,19 +2,36 @@ import Menu from "./components/menu.js";
 import Link from "./components/link.js";
 
 const generalLinks = [
-  new Link("About Us", "/#about"),
-  new Link("Coworking Types", "/#pricing"),
-  new Link("Contacts", "/#contacts"),
+  new Link("About Us", "https://midjiro.github.io/epam-coworking/#about"),
+  new Link(
+    "Coworking Types",
+    "https://midjiro.github.io/epam-coworking/#pricing"
+  ),
+  new Link("Contacts", "https://midjiro.github.io/epam-coworking/#contacts"),
 ];
 
 const defaultLinks = generalLinks.concat(
-  new Link("Authenticate", "templates/authenticate.html"),
-  new Link("Book a place", "/#booking-form", "button--outline")
+  new Link(
+    "Authenticate",
+    "https://midjiro.github.io/epam-coworking/templates/authenticate.html"
+  ),
+  new Link(
+    "Book a place",
+    "https://midjiro.github.io/epam-coworking/#booking-form",
+    "button--outline"
+  )
 );
 
 const authenticatedUserLinks = generalLinks.concat(
-  new Link("Profile", "templates/profile.html"),
-  new Link("Book a place", "/#booking-form", "button--outline")
+  new Link(
+    "Profile",
+    "https://midjiro.github.io/epam-coworking/templates/profile.html"
+  ),
+  new Link(
+    "Book a place",
+    "https://midjiro.github.io/epam-coworking/#booking-form",
+    "button--outline"
+  )
 );
 
 const menu = new Menu(
