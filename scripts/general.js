6203d3be66c5637c1886a1f9703509506fbea3ad
midjiro <miha.gulak@gmail.com>
2023-06-02 18:02:15 +0300

    fix navigation links

--- scripts/general.js
+++ scripts/general.js
@@ -4,16 +4,13 @@ import Link from "./components/link.js";
 const generalLinks = [
   new Link("About Us", "/#about"),
   new Link("Coworking Types", "/#pricing"),
-  new Link("Contacts", "/#contacts"),
 ];
 
 const defaultLinks = generalLinks.concat(
-  new Link("Authenticate", "templates/authenticate.html"),
-  new Link("Book a place", "/#booking-form", "button--outline")
+  new Link("Authenticate", "templates/authenticate.html")
 );
 
-const authenticatedUserLinks = generalLinks.concat(
-  new Link("Profile", "templates/profile.html"),
+const authenticatedUserLinks = defaultLinks.concat(
   new Link("Book a place", "/#booking-form", "button--outline")
 );
 
