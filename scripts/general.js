fa4f852389496dd63e769929a5108481479b4bc3
midjiro <miha.gulak@gmail.com>
2023-06-04 13:15:14 +0300

    fix bugs in js files, fix navigation links

--- scripts/general.js
+++ scripts/general.js
@@ -2,50 +2,30 @@ import Menu from "./components/menu.js";
 import Link from "./components/link.js";
 
 const generalLinks = [
-<<<<<<< HEAD
   new Link("About Us", "https://midjiro.github.io/epam-coworking/#about"),
   new Link(
     "Coworking Types",
     "https://midjiro.github.io/epam-coworking/#pricing"
   ),
-  new Link("Contacts", "https://midjiro.github.io/epam-coworking/#contacts"),
+  new Link(
+    "Contact Us",
+    "https://midjiro.github.io/epam-coworking/templates/contact.html"
+  ),
 ];
 
 const defaultLinks = generalLinks.concat(
   new Link(
     "Authenticate",
     "https://midjiro.github.io/epam-coworking/templates/authenticate.html"
-  ),
-  new Link(
-    "Book a place",
-    "https://midjiro.github.io/epam-coworking/#booking-form",
-    "button--outline"
   )
 );
 
 const authenticatedUserLinks = generalLinks.concat(
   new Link(
-    "Profile",
-    "https://midjiro.github.io/epam-coworking/templates/profile.html"
-  ),
-  new Link(
     "Book a place",
-    "https://midjiro.github.io/epam-coworking/#booking-form",
+    "https://midjiro.github.io/epam-coworking/templates/booking.html",
     "button--outline"
   )
-=======
-  new Link("About Us", "/#about"),
-  new Link("Coworking Types", "/#pricing"),
-  new Link("Contact Us", "/templates/contact.html"),
-];
-
-const defaultLinks = generalLinks.concat(
-  new Link("Authenticate", "/templates/authenticate.html")
-);
-
-const authenticatedUserLinks = generalLinks.concat(
-  new Link("Book a place", "/templates/booking.html", "button--outline")
->>>>>>> dev
 );
 
 const menu = new Menu(
